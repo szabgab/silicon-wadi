92a0e5f19116664f25d896becc542402ace069a7
Gabor Szabo <gabor@szabgab.com>
2017-05-10 10:32:53 +0300

    first few companies

---
+++ README.md
@@ -0,0 +1,3 @@
+List of hi-tech companies in Israel along with the location of their development offices
+and if possible along with a list of technologies they are using.
+
---
+++ tidy_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import json
+import glob
+
+# format the json files
+
+for filename in glob.glob("companies.json"):
+    with open(filename) as fh:
+        data = json.load(fh)
+    with open(filename, 'w') as fh:
+        data.sort(key = lambda c: c['name'])
+        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '))
+
+# vim: expandtab
