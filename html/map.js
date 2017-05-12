110e72f28c8cf1477309324a33d7651a6741c517
Gabor Szabo <gabor@szabgab.com>
2017-05-12 08:48:43 +0300

    filter markers based on technology

--- html/map.js
+++ html/map.js
@@ -1,6 +1,9 @@
 var companies = [];
 
 function show_map() {
+    var t = document.getElementById('technology')
+    var technology = t.options[t.selectedIndex].value;
+    //console.log(technology);
     var xcenter = {lat: 32.1665615, lng: 34.81151369999998};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 9,
@@ -10,19 +13,44 @@ function show_map() {
     for (var i=0; i < companies.length; i++) {
         for (var j=0; j < companies[i]['offices'].length; j++) {
             //console.log(companies[i]['offices'][j]);
-            if (companies[i]['offices'][j]['coordinates']) {
-               console.log(companies[i]['offices'][j]);
-                var marker = new google.maps.Marker({
-                    position: companies[i]['offices'][j]['coordinates'],
-                    title: companies[i]['name'],
-                    map: map
-                });
+            if (! companies[i]['offices'][j]['coordinates']) {
+                continue;
             }
+
+            if (technology) {
+                if (! companies[i]['technologies']) {
+                    continue;
+                }
+                var hits = companies[i]['technologies'].filter(function(v) {
+                    return technology == v
+                })
+                if (hits.length == 0) {
+                    continue;
+                }
+            }
+
+            //console.log(companies[i]['offices'][j]);
+            var marker = new google.maps.Marker({
+                position: companies[i]['offices'][j]['coordinates'],
+                title: companies[i]['name'],
+                map: map
+            });
         }
     }
 }
 
 function initMap() {
+    document.getElementById('show').addEventListener('click', show_map);
+
+    ajax_get('/data/technologies.json', function(data) {
+       //technologies = data;
+       var html = "<option></option>";
+       for (var i=0; i < data.length; i++) {
+           html += "<option>" + data[i] + "</option>";
+       }
+       document.getElementById('technology').innerHTML = html;
+    });
+
    ajax_get('/data/companies.json', function(data) {
        //console.log(data);
        companies = data;
@@ -31,7 +59,7 @@ function initMap() {
 }
 
 function ajax_get(url, callback) {
-    xmlhttp = new XMLHttpRequest();
+    var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             //console.log('responseText:' + xmlhttp.responseText);
