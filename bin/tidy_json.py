8e8fe12c26a0b4ee9a780162b2655ca003c79e19
Gabor Szabo <gabor@szabgab.com>
2025-11-06 12:44:50 +0200

    better error message

--- bin/tidy_json.py
+++ bin/tidy_json.py
@@ -8,23 +8,28 @@ data_dir = 'html/data/'
 def tidy(test = False):
     for filename in glob.glob(data_dir + "*.json") + glob.glob(data_dir + 'c/' + "*.json"):
         #print(filename)
-        with open(filename) as fh:
-            json_str = fh.read()
-            data = json.loads(json_str)
+        try:
+            with open(filename) as fh:
+                json_str = fh.read()
+                data = json.loads(json_str)
 
-#        if filename == data_dir + 'companies.json':
-#            data.sort(key = lambda c: c['name'])
-        if filename == data_dir + 'technologies.json':
-            data.sort()
 
-        new_json_str = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
-        if test:
-            assert new_json_str == json_str, filename + " is not tidy. Run 'python bin/tidy_json.py'"
-            continue
+    #        if filename == data_dir + 'companies.json':
+    #            data.sort(key = lambda c: c['name'])
+            if filename == data_dir + 'technologies.json':
+                data.sort()
 
-        with open(filename, 'w') as fh:
-            fh.write(new_json_str)
-            
+            new_json_str = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+            if test:
+                assert new_json_str == json_str, filename + " is not tidy. Run 'python bin/tidy_json.py'"
+                continue
+
+            with open(filename, 'w') as fh:
+                fh.write(new_json_str)
+        except Exception as err:
+            print(f"Exception {err} in file {filename}")
+            exit(1)
+
 
 if __name__ == "__main__":
     tidy()
